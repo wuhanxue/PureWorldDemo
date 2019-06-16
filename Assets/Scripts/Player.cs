22a5949d7ef0f5c3919e66f82ee10ba6d951db1e
MattJasonLu <1132464757@qq.com>
2019-06-16 11:13:22 +0900

    小人跑动完成

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,36 +6,38 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.5f;
     private Animator animator;
-
+    private SpriteRenderer spriteRenderer;
+    private float horizontal;
+    private float vertical;
     private void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        //Debug.Log("horizontal" + horizontal);
-        //Debug.Log("vertical" + vertical);
-        Debug.Log("Direction: " +  animator.GetInteger("Direction") );
-        if (horizontal > 0)
+        Move();
+        if (vertical > 0) animator.SetInteger("Walk", -1);
+        else if (vertical < 0) animator.SetInteger("Walk", 1);
+        else if (horizontal > 0)
         {
-            animator.SetInteger("Direction", 2);
+            animator.SetInteger("Walk", -2);
+            spriteRenderer.flipX = false;
         }
         else if (horizontal < 0)
         {
-            animator.SetInteger("Direction", 3);
+            animator.SetInteger("Walk", 2);
+            spriteRenderer.flipX = true;
         }
-        if (vertical > 0)
-        {
-            animator.SetInteger("Direction", 1);
-        }
-        else if (vertical < 0)
-        {
-            animator.SetInteger("Direction", 0);
-        }
+        else animator.SetInteger("Walk", 0);
+
+    }
+    private void Move()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
         transform.Translate(Vector2.up * vertical * moveSpeed * Time.deltaTime);
         transform.Translate(Vector2.right * horizontal * moveSpeed * Time.deltaTime);
     }
